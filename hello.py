952d1e5cb1bbc587dc85563348c38a8e021ea857
@Mojonario <84666201+Blacksai1l@users.noreply.github.com>
2024-04-28 15:39:32 +0200

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,20 @@
+
+# Lo que va tras una almohadilla son comentarios para ir entendiendo el codigo.
+
+# voy a picar mi primer código con un hola mundo
+print ("Hola, mundo")
+
+
+"""
+esto es 
+un comentario 
+en tres lineas
+"""
+
+#el comando type te permite saber que tipo de variable es
+
+print (type ("hola, mundo")) # cadeba de texto string
+print (type (3)) # numero entero
+print (type (3.5)) # numero float 
+print (type ( 1 + 3j)) # numero complejo
+ 
