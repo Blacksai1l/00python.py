ddd76d5f25dd24f0cf1ae9ab21722509b1cefdaa
@Mojonario <manucastillamartino@gmail.com>
2024-06-23 20:56:59 +0200

    Changes to be committed: 	new file:   python.py/Backend/type_hints.py 	modified:   rust/target/.rustc_info.json

---
+++ python.py/Backend/type_hints.py
@@ -0,0 +1,23 @@
+#### type hints #####
+
+miString = "mi variable string"
+
+print(miString)
+print(type (miString))
+
+miString = 5
+
+print(miString)
+print(type (miString))
+
+# acabo de cambiar el tipo de variable de string a int en tiempo de ejecucion , por eso python es de tipado dinamico y debil
+
+miOtrostring = str = "mi string tipada"
+
+print (miOtrostring)
+print (type(miOtrostring))
+
+miOtrostring = int = 5
+
+print (miOtrostring)
+print (type(miOtrostring))
