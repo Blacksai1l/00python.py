493fbce9af927c09ea977ea053b483ea40e11106
@Mojonario <manucastillamartino@gmail.com>
2024-06-23 04:10:21 +0200

    Changes to be committed: 	deleted:    rust.rs/hello-world/Cargo.lock 	deleted:    rust.rs/hello-world/Cargo.toml 	deleted:    rust.rs/hello-world/src/00holamuindo.rs 	deleted:    rust.rs/hello-world/src/main.rs 	deleted:    rust.rs/hello-world/target/.rustc_info.json 	deleted:    rust.rs/hello-world/target/CACHEDIR.TAG 	deleted:    rust.rs/hello-world/target/debug/.cargo-lock 	deleted:    rust.rs/hello-world/target/debug/.fingerprint/hello-world-9d6a8688aae086be/invoked.timestamp 	deleted:    rust.rs/hello-world/target/debug/.fingerprint/hello-world-9d6a8688aae086be/output-bin-Hola Rust 	deleted:    rust.rs/hello-world/target/debug/.fingerprint/hello-world-9f31ad923f592c85/dep-test-bin-hello-world 	deleted:    rust.rs/hello-world/target/debug/.fingerprint/hello-world-9f31ad923f592c85/invoked.timestamp 	deleted:    rust.rs/hello-world/target/debug/.fingerprint/hello-world-9f31ad923f592c85/test-bin-hello-world 	deleted:    rust.rs/hello-world/target/debug/.fingerprint/hello-world-9f31ad923f592c85/test-bin-hello-world.json 	deleted:    rust.rs/hello-world/target/debug/.fingerprint/hello-world-aa8a742b62bd37bd/bin-hello-world 	deleted:    rust.rs/hello-world/target/debug/.fingerprint/hello-world-aa8a742b62bd37bd/bin-hello-world.json 	deleted:    rust.rs/hello-world/target/debug/.fingerprint/hello-world-aa8a742b62bd37bd/dep-bin-hello-world 	deleted:    rust.rs/hello-world/target/debug/.fingerprint/hello-world-aa8a742b62bd37bd/invoked.timestamp 	deleted:    rust.rs/hello-world/target/debug/deps/hello_world-9f31ad923f592c85.d 	deleted:    rust.rs/hello-world/target/debug/deps/hello_world-aa8a742b62bd37bd.d 	deleted:    rust.rs/hello-world/target/debug/deps/libhello_world-9f31ad923f592c85.rmeta 	deleted:    rust.rs/hello-world/target/debug/deps/libhello_world-aa8a742b62bd37bd.rmeta 	deleted:    rust.rs/hello-world/target/debug/incremental/hello_world-1ui7nyuhbye0s/s-gxdepuajdr-e01oju-2jpejoubyrmzjcadqor9o4m3c/dep-graph.bin 	deleted:    rust.rs/hello-world/target/debug/incremental/hello_world-1ui7nyuhbye0s/s-gxdepuajdr-e01oju-2jpejoubyrmzjcadqor9o4m3c/query-cache.bin 	deleted:    rust.rs/hello-world/target/debug/incremental/hello_world-1ui7nyuhbye0s/s-gxdepuajdr-e01oju-2jpejoubyrmzjcadqor9o4m3c/work-products.bin 	deleted:    rust.rs/hello-world/target/debug/incremental/hello_world-1ui7nyuhbye0s/s-gxdepuajdr-e01oju.lock 	deleted:    rust.rs/hello-world/target/debug/incremental/hello_world-lrg7lrqr898x/s-gxdepuakgm-23v4k3-dd42afbu14cgwlhpct74hftsz/dep-graph.bin 	deleted:    rust.rs/hello-world/target/debug/incremental/hello_world-lrg7lrqr898x/s-gxdepuakgm-23v4k3-dd42afbu14cgwlhpct74hftsz/query-cache.bin 	deleted:    rust.rs/hello-world/target/debug/incremental/hello_world-lrg7lrqr898x/s-gxdepuakgm-23v4k3-dd42afbu14cgwlhpct74hftsz/work-products.bin 	deleted:    rust.rs/hello-world/target/debug/incremental/hello_world-lrg7lrqr898x/s-gxdepuakgm-23v4k3.lock

--- rust.rs/hello-world/target/debug/deps/hello_world-9f31ad923f592c85.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Manu\Documents\GitHub\proyecto\PROYECTOS\rust.rs\hello-world\target\debug\deps\libhello_world-9f31ad923f592c85.rmeta: src/main.rs
-
-C:\Users\Manu\Documents\GitHub\proyecto\PROYECTOS\rust.rs\hello-world\target\debug\deps\hello_world-9f31ad923f592c85.d: src/main.rs
-
-src/main.rs:
