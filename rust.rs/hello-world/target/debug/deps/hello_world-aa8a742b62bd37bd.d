b83a536589d50c63f51414fdddcfd9beab3f25ee
@Mojonario <manucastillamartino@gmail.com>
2024-06-23 03:41:00 +0200

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   rust.rs/hello-world/Cargo.lock
    	new file:   rust.rs/hello-world/Cargo.toml
    	new file:   rust.rs/hello-world/src/main.rs
    	new file:   rust.rs/hello-world/target/.rustc_info.json
    	new file:   rust.rs/hello-world/target/CACHEDIR.TAG
    	new file:   rust.rs/hello-world/target/debug/.cargo-lock
    	new file:   rust.rs/hello-world/target/debug/.fingerprint/hello-world-9f31ad923f592c85/dep-test-bin-hello-world
    	new file:   rust.rs/hello-world/target/debug/.fingerprint/hello-world-9f31ad923f592c85/invoked.timestamp
    	new file:   rust.rs/hello-world/target/debug/.fingerprint/hello-world-9f31ad923f592c85/test-bin-hello-world
    	new file:   rust.rs/hello-world/target/debug/.fingerprint/hello-world-9f31ad923f592c85/test-bin-hello-world.json
    	new file:   rust.rs/hello-world/target/debug/.fingerprint/hello-world-aa8a742b62bd37bd/bin-hello-world
    	new file:   rust.rs/hello-world/target/debug/.fingerprint/hello-world-aa8a742b62bd37bd/bin-hello-world.json
    	new file:   rust.rs/hello-world/target/debug/.fingerprint/hello-world-aa8a742b62bd37bd/dep-bin-hello-world
    	new file:   rust.rs/hello-world/target/debug/.fingerprint/hello-world-aa8a742b62bd37bd/invoked.timestamp
    	new file:   rust.rs/hello-world/target/debug/deps/hello_world-9f31ad923f592c85.d
    	new file:   rust.rs/hello-world/target/debug/deps/hello_world-aa8a742b62bd37bd.d
    	new file:   rust.rs/hello-world/target/debug/deps/libhello_world-9f31ad923f592c85.rmeta
    	new file:   rust.rs/hello-world/target/debug/deps/libhello_world-aa8a742b62bd37bd.rmeta
    	new file:   rust.rs/hello-world/target/debug/incremental/hello_world-1ui7nyuhbye0s/s-gxdepuajdr-e01oju-2jpejoubyrmzjcadqor9o4m3c/dep-graph.bin
    	new file:   rust.rs/hello-world/target/debug/incremental/hello_world-1ui7nyuhbye0s/s-gxdepuajdr-e01oju-2jpejoubyrmzjcadqor9o4m3c/query-cache.bin
    	new file:   rust.rs/hello-world/target/debug/incremental/hello_world-1ui7nyuhbye0s/s-gxdepuajdr-e01oju-2jpejoubyrmzjcadqor9o4m3c/work-products.bin
    	new file:   rust.rs/hello-world/target/debug/incremental/hello_world-1ui7nyuhbye0s/s-gxdepuajdr-e01oju.lock
    	new file:   rust.rs/hello-world/target/debug/incremental/hello_world-lrg7lrqr898x/s-gxdepuakgm-23v4k3-dd42afbu14cgwlhpct74hftsz/dep-graph.bin
    	new file:   rust.rs/hello-world/target/debug/incremental/hello_world-lrg7lrqr898x/s-gxdepuakgm-23v4k3-dd42afbu14cgwlhpct74hftsz/query-cache.bin
    	new file:   rust.rs/hello-world/target/debug/incremental/hello_world-lrg7lrqr898x/s-gxdepuakgm-23v4k3-dd42afbu14cgwlhpct74hftsz/work-products.bin
    	new file:   rust.rs/hello-world/target/debug/incremental/hello_world-lrg7lrqr898x/s-gxdepuakgm-23v4k3.lock

---
+++ rust.rs/hello-world/target/debug/deps/hello_world-aa8a742b62bd37bd.d
@@ -0,0 +1,5 @@
+C:\Users\Manu\Documents\GitHub\proyecto\PROYECTOS\rust.rs\hello-world\target\debug\deps\libhello_world-aa8a742b62bd37bd.rmeta: src/main.rs
+
+C:\Users\Manu\Documents\GitHub\proyecto\PROYECTOS\rust.rs\hello-world\target\debug\deps\hello_world-aa8a742b62bd37bd.d: src/main.rs
+
+src/main.rs:
