ddd6a84ec58360a71a66c6a52a6efe2852b23e2a
@Mojonario <manucastillamartino@gmail.com>
2024-06-23 03:32:39 +0200

    Changes to be committed: 	deleted:    rust.rs/hello-rust/Cargo.lock 	deleted:    rust.rs/hello-rust/Cargo.toml 	deleted:    rust.rs/hello-rust/src/main.rs 	deleted:    rust.rs/hello-rust/target/.rustc_info.json 	deleted:    rust.rs/hello-rust/target/CACHEDIR.TAG 	deleted:    rust.rs/hello-rust/target/debug/.cargo-lock 	deleted:    rust.rs/hello-rust/target/debug/.fingerprint/hello-rust-0c38bcbf72a4f500/bin-hello-rust 	deleted:    rust.rs/hello-rust/target/debug/.fingerprint/hello-rust-0c38bcbf72a4f500/bin-hello-rust.json 	deleted:    rust.rs/hello-rust/target/debug/.fingerprint/hello-rust-0c38bcbf72a4f500/dep-bin-hello-rust 	deleted:    rust.rs/hello-rust/target/debug/.fingerprint/hello-rust-0c38bcbf72a4f500/invoked.timestamp 	deleted:    rust.rs/hello-rust/target/debug/.fingerprint/hello-rust-d7c5330a789649aa/dep-test-bin-hello-rust 	deleted:    rust.rs/hello-rust/target/debug/.fingerprint/hello-rust-d7c5330a789649aa/invoked.timestamp 	deleted:    rust.rs/hello-rust/target/debug/.fingerprint/hello-rust-d7c5330a789649aa/test-bin-hello-rust 	deleted:    rust.rs/hello-rust/target/debug/.fingerprint/hello-rust-d7c5330a789649aa/test-bin-hello-rust.json 	deleted:    rust.rs/hello-rust/target/debug/deps/hello_rust-0c38bcbf72a4f500.d 	deleted:    rust.rs/hello-rust/target/debug/deps/hello_rust-d7c5330a789649aa.d 	deleted:    rust.rs/hello-rust/target/debug/deps/libhello_rust-0c38bcbf72a4f500.rmeta 	deleted:    rust.rs/hello-rust/target/debug/deps/libhello_rust-d7c5330a789649aa.rmeta 	deleted:    rust.rs/hello-rust/target/debug/incremental/hello_rust-3h4y5qiez0ahl/s-gxde9zohg4-z5tcca-e74ovkvfzlhej5wmgcecs6kal/dep-graph.bin 	deleted:    rust.rs/hello-rust/target/debug/incremental/hello_rust-3h4y5qiez0ahl/s-gxde9zohg4-z5tcca-e74ovkvfzlhej5wmgcecs6kal/query-cache.bin 	deleted:    rust.rs/hello-rust/target/debug/incremental/hello_rust-3h4y5qiez0ahl/s-gxde9zohg4-z5tcca-e74ovkvfzlhej5wmgcecs6kal/work-products.bin 	deleted:    rust.rs/hello-rust/target/debug/incremental/hello_rust-3h4y5qiez0ahl/s-gxde9zohg4-z5tcca.lock 	deleted:    rust.rs/hello-rust/target/debug/incremental/hello_rust-3i0efl5g30mbn/s-gxde9zoiny-13xpcx2-6not16xiwlpna3max2vtwwtvm/dep-graph.bin 	deleted:    rust.rs/hello-rust/target/debug/incremental/hello_rust-3i0efl5g30mbn/s-gxde9zoiny-13xpcx2-6not16xiwlpna3max2vtwwtvm/query-cache.bin 	deleted:    rust.rs/hello-rust/target/debug/incremental/hello_rust-3i0efl5g30mbn/s-gxde9zoiny-13xpcx2-6not16xiwlpna3max2vtwwtvm/work-products.bin 	deleted:    rust.rs/hello-rust/target/debug/incremental/hello_rust-3i0efl5g30mbn/s-gxde9zoiny-13xpcx2.lock

--- rust.rs/hello-rust/target/debug/deps/hello_rust-0c38bcbf72a4f500.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Manu\Documents\GitHub\python.py\PROYECTOS\rust.rs\hello-rust\target\debug\deps\libhello_rust-0c38bcbf72a4f500.rmeta: src/main.rs
-
-C:\Users\Manu\Documents\GitHub\python.py\PROYECTOS\rust.rs\hello-rust\target\debug\deps\hello_rust-0c38bcbf72a4f500.d: src/main.rs
-
-src/main.rs:
--- rust.rs/hello-rust/target/debug/deps/hello_rust-d7c5330a789649aa.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Manu\Documents\GitHub\python.py\PROYECTOS\rust.rs\hello-rust\target\debug\deps\libhello_rust-d7c5330a789649aa.rmeta: src/main.rs
-
-C:\Users\Manu\Documents\GitHub\python.py\PROYECTOS\rust.rs\hello-rust\target\debug\deps\hello_rust-d7c5330a789649aa.d: src/main.rs
-
-src/main.rs:
