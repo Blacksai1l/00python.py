ccc9f7b5f935873e6e70824c5105268aebb9114e
@Mojonario <manucastillamartino@gmail.com>
2024-06-24 00:01:08 +0200

    Changes to be committed: 	modified:   python.py/Backend/FastAPI/main.py

--- python.py/Backend/FastAPI/main.py
+++ python.py/Backend/FastAPI/main.py
@@ -1,12 +1,16 @@
+
+
+"""siempre que llamamos a un servidor la funcion tiene que ser asincrona. async. 
+Todas las aplicaciones que estan en un servidor estan asincronicas de manera 
+default hasta que necesitan sincronizarse"""
+
 from fastapi import FastAPI
 
 app = FastAPI ()
 
 @app.get ("/")
 async def root (): 
-    """siempre que llamamos a un servidor la funcion tiene que ser asincrona. async. 
-    Todas las aplicaciones que estan en un servidor estan asincronicas de manera 
-    default hasta que necesitan sincronizarse"""
+
     return "hola, FasAPI!"
 
 # vamos ahora a levantar el servidos que nos proporciona fastapi
