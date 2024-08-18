2094811a61556a812d841fed4b3c7e088e4d21ec
@Mojonario <manucastillamartino@gmail.com>
2024-08-18 17:49:14 +0200

    On branch main  Your branch is ahead of 'origin/main' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   mi_projecto_chia/main.py
    
     Changes not staged for commit:
    	modified:   mi_projecto_chia/__pycache__/main.cpython-312.pyc
    	modified:   python.py/Backend/FastAPI/routers/autentificacion.py
    	modified:   rust/target/.rustc_info.json
    
     Untracked files:
    	tail-database-app/

--- mi_projecto_chia/main.py
+++ mi_projecto_chia/main.py
@@ -1,39 +1,14 @@
-from fastapi import FastAPI, Query
-import requests
+# main.py
+from fastapi import FastAPI
+import httpx
 
 app = FastAPI()
 
-DEXIE_API_URL = "https://api.dexie.space/v2/prices/pairs"
-DEXIE_API_URL2 = "https://api.dexie.space/v2/prices/tickers"
-
-@app.get("/tokens")
-def get_chia_tokens(base: str = Query(None, description="Base del token para filtrar")):
-    """
-    Consulta la lista de tokens CATS desde la API de Dexie y filtra por el campo 'base' si se proporciona.
-    """
-    try:
-        response = requests.get(DEXIE_API_URL)
-        response.raise_for_status()  # Asegurarse de que la solicitud fue exitosa
+@app.get("/api/tokens")
+async def get_tokens():
+    async with httpx.AsyncClient() as client:
+        response = await client.get("https://api.v2.tibetswap.io/tokens")
         tokens = response.json()
+    return tokens
 
-        if base:
-            # Filtrar tokens por base (case-insensitive)
-            filtered_tokens = [token for token in tokens if token['base'].lower() == base.lower()]
-            return filtered_tokens if filtered_tokens else {"error": f"No se encontró ningún token con base '{base}'."}
-
-        return tokens  # Devuelve todos los tokens si no se proporciona un filtro
-    except requests.exceptions.RequestException as e:
-        return {"error": str(e)}
-    
-@app.get("/prices")
-def get_chia_prices():
-    """
-    Consulta la lista de precios de tokens desde la API de Dexie.
-    """
-    try:
-        response = requests.get(DEXIE_API_URL2)
-        response.raise_for_status()
-        prices = response.json()
-        return prices
-    except requests.exceptions.RequestException as e:
-        return {"error": str(e)}
+# Para ejecutar: uvicorn main:app --reload
